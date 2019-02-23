45880c8511d802b2710ab79cad36caac844ee265
Ken Campbell <ken@campbellmusclelab.org>
2019-02-23 13:26:15 -0500

    Adding in demos

---
+++ demo_files/demo_test_1.py
@@ -0,0 +1,19 @@
+# Code for testing growth
+import numpy as np
+import pandas as pd
+try:
+    import Python_single_ventricle as sc
+    import Python_MyoSim.half_sarcomere as myosim
+    import untangle as ut
+except:
+    import sys
+    print(sys.path)
+    sys.path.append('c:\\ken\\github\\campbellmusclelab\\python\\modules')
+    import Python_single_ventricle as sc
+    import Python_MyoSim.half_sarcomere as myosim
+
+# Set path to xml file
+xml_file_string = 'input_data\\test_human_1.xml'
+
+# Run simulation
+sc.run_simulation_from_xml_file(xml_file_string)
