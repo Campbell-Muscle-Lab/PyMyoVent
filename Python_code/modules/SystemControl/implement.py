99b3b4b20dc25063da34e4282771f4e087de4f6f
Hossein <42911295+HosseinGit@users.noreply.github.com>
2019-12-15 18:52:16 -0500

    SystemControl
    New module named "system_control" responsible for running the baroreceptor has been adopted.

---
+++ Python_code/modules/SystemControl/implement.py
@@ -0,0 +1,216 @@
+import numpy as np
+from math import *
+from scipy.integrate import solve_ivp
+from scipy import signal
+
+def update_baroreceptor(self,time_step,arterial_pressure,arterial_pressure_rate,i):
+
+    if (self.baro_scheme == "simple_model"):
+        # baroreceptor control
+        self.bc = self.bc_min+self.bc_max*exp((arterial_pressure-self.P_n)/self.slope))\
+            /(1+exp((arterial_pressure-self.P_n)/self.slope))
+
+
+    if (self.baro_scheme=="Ursino_1998"):
+
+        #print('arterial_pressure_rate',arterial_pressure_rate)
+        def P_tilda_derive(y,t):
+            dy=np.zeros(1)
+            dy[0]=1/self.tau_p*(arterial_pressure+self.tau_z*arterial_pressure_rate-self.P_tilda)
+            return dy
+            #determine the output variable of the linear dynamic block
+            #print('self.P_tilda',self.P_tilda)
+        sol=solve_ivp(P_tilda_derive,[0,time_step],[self.P_tilda])#self.P_tilda)
+        self.P_tilda=sol.y[:,-1]
+
+        #determine the frequency of spikes in the affetent pathway
+        self.f_cs=(self.f_min+self.f_max*exp((self.P_tilda-self.P_n)/self.k_a))\
+            /(1+exp((self.P_tilda-self.P_n)/self.k_a))
+
+        #determine the frequency of spikes in the efferent sympathetic pathway
+        self.f_es=np.append(self.f_es,\
+                (self.f_es_inf+(self.f_es_0-self.f_es_inf)*exp(-self.k_es*self.f_cs)))
+        self.f_es_min=(self.f_es_inf+(self.f_es_0-self.f_es_inf)*exp(-self.k_es*self.f_max))
+        #print(self.f_es[i])
+        #determine the frequency of spikes in the efferent vagal pathway
+        self.f_ev=np.append(self.f_ev,\
+        (self.f_ev_0+self.f_ev_inf*exp((self.f_cs-self.f_cs_0)/self.k_ev))\
+            /(1+exp((self.f_cs-self.f_cs_0)/self.k_ev)))
+        #return self.P_tilda,self.f_cs,self.f_es,self.f_es_min,self.f_ev
+def return_heart_period(self,time_step,i):
+
+    if self.baro_scheme == "simple_model"):
+        self.return_heart_period_control(self,time_step,i)
+        self.baroreceptor_counter -= 1
+        if (self.baroreceptor_counter <= -self.counter_systole):
+            self.T=self.T_prime
+            self.T_diastole = self.T-self.T_systole
+            self.counter_diastole = int(self.T_diastole/self.dt)
+            self.baroreceptor_counter = self.counter_diastole
+    if self.baro_scheme == "Ursino_1998"):
+        self.return_heart_period_control(self,time_step,i)
+        self.baroreceptor_counter -= 1
+        if (self.baroreceptor_counter <= -self.counter_systole):
+            self.T=self.T_prime
+            self.T_diastole = self.T-self.T_systole
+            self.counter_diastole = int(self.T_diastole/self.dt)
+            self.baroreceptor_counter = self.counter_diastole
+
+def return_contractility(self,time_step,i):
+    if self.baro_scheme == "Ursino_1998"):
+        self.return_contractility_control(self,time_step,i)
+        self.baroreceptor_counter -= 1
+def return_heart_period_control(self,time_step,i):
+
+    if (self.baro_scheme == "simple_model"):
+        def derivs(y,t):
+            delta_T_prime=np.zeros(0)
+            delta_T_prime[0] = self.G_T*(self.bc-self.bc)
+            return delta_T_prime
+
+        sol = solve_ivp(derivs,[0,time_step],self.delta_T_prime)
+        self.delta_T_prime=sol.y[:,-1]
+        self.T_prime=self.T_prime+self.delta_T_prime
+
+    if (self.baro_scheme=="Ursino_1998"):
+        #determine the outputs of static characteristics (steady-state changes)
+        # apply time delay
+        if i < self.D_Ts:
+            delay_Ts=int(0)
+        else:
+            delay_Ts=self.D_Ts
+        #sympathetic
+        if self.f_es[i] >= self.f_es_min:
+            sigma_Ts=self.G_Ts*log(self.f_es[i-delay_Ts]-self.f_es_min+1)
+        else:
+            sigma_Ts=0.0
+
+        # apply time delay
+        if i < self.D_Tv:
+            delay_Tv=int(0)
+        else:
+            delay_Tv=self.D_Tv
+        #vagal
+        sigma_Tv=self.G_Tv*self.f_ev[i-delay_Tv]
+
+        #determine the heart rate change due to sympathetic and vagal activities
+        def delta_T_derives(y,t):
+
+            delta_T=np.zeros(2)
+            #delta_Ts_deriv
+            delta_T[0]=1/self.tau_Ts*(sigma_Ts-self.delta_Ts)
+            #delta_Tv_deriv
+            delta_T[1]=1/self.tau_Tv*(sigma_Tv-self.delta_Tv)
+            return delta_T
+
+        sol=solve_ivp(delta_T_derives,[0,time_step], [self.delta_Ts,self.delta_Tv])
+        self.y=sol.y[:,-1]
+        #print(self.y)
+        self.delta_Ts=self.y[0]
+        self.delta_Tv=self.y[1]
+
+        self.T_prime=self.delta_Ts+self.delta_Tv+self.T_prime
+
+    return self.T, self.P_tilda,self.f_cs,self.f_es[-1],self.f_es_min,self.f_ev[-1],self.delta_Ts,self.delta_Tv
+
+def return_contractility_control(self,time_step,i):
+
+    if (self.baro_scheme == "simple_model"):
+        def derivs(y,t):
+            delta_c_prime=np.zeros(0)
+            delta_c_prime[0] = self.G_k1*(self.bc-self.bc)
+            return delta_c_prime
+
+        sol = solve_ivp(derivs,[0,time_step],self.delta_c_prime)
+        self.delta_c_prime=sol.y[:,-1]
+        self.c_prime=self.c_prime+self.delta_c_prime
+
+    if (self.baro_scheme=="Ursino_1998"):
+        #update_baroreceptor(self,time_step,arterial_pressure,arterial_pressure_rate)
+        #determine the outputs of static characteristics (steady-state changes)
+        if i<self.D_E:
+            delay_E=int(0)
+        else:
+            delay_E=self.D_E
+        if self.f_es[i] >= self.f_es_min:
+            sigma_E=self.G_E*log(self.f_es[i-delay_E]-self.f_es_min+1)
+            sigma_L_factor=self.G_L*log(self.f_es[i]-self.f_es_min+1)
+            sigma_k_3_factor=self.G_k3*log(self.f_es[i]-self.f_es_min+1)
+            sigma_k_cb_factor=self.G_kcb*log(self.f_es[i]-self.f_es_min+1)
+            sigma_k_force_factor=self.G_k_force*log(self.f_es[i]-self.f_es_min+1)
+        else:
+            sigma_E=0.0
+            sigma_L_factor=0
+            sigma_k_3_factor=0
+            sigma_k_cb_factor=0
+            sigma_k_force_factor=0
+        #determine the elastance rate change due to sympathetic
+        def derivs(y,t):
+            delta=np.zeros(5)
+            delta[0]=1/self.tau_E*(sigma_E-y)
+            delta[1]=1/self.tau_L*(sigma_L_factor-y)
+            delta[2]=1/self.tau_k3*(sigma_k_3_factor-y)
+            delta[3]=1/self.tau_kcb*(sigma_k_cb_factor-y)
+            delta[4]=1/self.tau_k_force*(sigma_k_force_factor-y)
+            return delta
+        initial_values=np.zeros(5)
+        initial_values[0]=self.delta_E0
+        sol=solve_ivp(derivs,[0,time_step], initial_values)
+        y=sol.y[:,-1]
+        delta_E=y[0]
+        delta_L=y[1]
+        delta_k3=y[2]
+        delta_kcb=y[3]
+        delta_k_force=y[4]
+
+        #print('delta_E',delta_E)
+        E=delta_E+self.E_0
+        self.L_scale_factor=delta_L+1
+        self.k_3_scale_factor=delta_k3+1
+        self.k_cb_scale_factor=delta_kcb+1
+        self.k_force_scale_factor=delta_k_force+1
+
+    return self.L_scale_factor, self.k_3_scale_factor, self.k_cb_scale_factor,self.k_force_scale_factor
+
+def return_activation(self):
+    if (self.baro_scheme=="fixed_heart_rate"):
+        #print('fixed heart rate is activated')
+        predefined_activation_level =\
+            0.5*(1+signal.square(np.pi+2*np.pi*self.activation_frequency*self.t,
+            duty=self.activation_duty_ratio))
+        self.activation_level = predefined_activation_level[self.activation_counter]
+        self.activation_counter += 1
+        return self.activation_level
+
+    if (self.baro_scheme=="Ursino_1998"):
+
+        self.baroreceptor_counter -= 1
+        if (self.baroreceptor_counter <= 0):
+            self.activation_level = 1.0
+        if (self.baroreceptor_counter <= -self.counter_systole):
+            self.activation_level = 0.0
+            self.T_diastole = self.T-self.T_systole
+            self.counter_diastole = int(self.T_diastole/self.dt)
+            self.baroreceptor_counter = self.counter_diastole
+        return self.activation_level
+
+def update_data_holder(self,time_step):
+
+    # Update data struct for system-control
+    self.sys_time = self.sys_time + time_step
+    self.data_buffer_index += 1
+
+    self.sys_data.at[self.data_buffer_index, 'heart_period']=self.T_prime
+    self.sys_data.at[self.data_buffer_index, 'T_prime']=self.T_prime
+    self.sys_data.at[self.data_buffer_index, 'P_tilda'] = self.P_tilda
+    self.sys_data.at[self.data_buffer_index, 'f_cs'] = self.f_cs
+#    self.sys_data.at[self.data_buffer_index, 'f_es'] = self.f_es[-1]
+#    self.sys_data.at[self.data_buffer_index, 'f_es_min'] = self.f_es_min
+#    self.sys_data.at[self.data_buffer_index, 'f_ev'] = self.f_ev[-1]
+#    self.sys_data.at[self.data_buffer_index, 'delta_Ts'] = self.delta_Ts
+#    self.sys_data.at[self.data_buffer_index, 'delta_Tv'] = self.delta_Tv
+#    self.sys_data.at[self.data_buffer_index, 'LV_elastance'] = self.E
+    self.sys_data.at[self.data_buffer_index, 'L_scale_factor'] = self.L_scale_factor
+    self.sys_data.at[self.data_buffer_index, 'k_3_scale_factor'] = self.k_3_scale_factor
+    self.sys_data.at[self.data_buffer_index, 'k_cb_scale_factor'] = self.k_cb_scale_factor
+    self.sys_data.at[self.data_buffer_index, 'k_force_scale_factor'] = self.k_force_scale_factor
---
+++ Python_code/modules/SystemControl/system_control.py
@@ -0,0 +1,170 @@
+import numpy as np
+import pandas as pd
+
+
+class system_control():
+    """Class for baroreflex"""
+
+    from .implement import update_baroreceptor,return_heart_period,return_contractility
+    from .implement import return_activation, update_data_holder
+
+
+    def __init__(self,baro_params,data_buffer_size): #baro_params
+        """input constatn parameters"""
+        baroreflex=baro_params#single_circulation_simulation.baroreflex
+        self.baro_scheme=baroreflex.baro_scheme.cdata
+
+        if (self.baro_scheme=="fixed_heart_rate"):
+
+            """bring the activation function to this section"""
+            self.T=float(baroreflex.simulation.basal_heart_period.cdata)
+            self.no_of_time_points = \
+                int(baroreflex.simulation.no_of_time_points.cdata)
+            self.dt = float(baroreflex.simulation.time_step.cdata)
+            self.activation_frequency = float(1/self.T)
+            self.activation_duty_ratio = \
+                    float(baroreflex.simulation.duty_ratio.cdata)
+            self.t = self.dt*np.arange(1, self.no_of_time_points+1)
+
+            self.activation_counter=int(0)
+            self.sys_data = pd.DataFrame({})
+        if (self.baro_scheme == "simple_model"):
+            #Activation function
+            self.dt=float(baroreflex.simulation.time_step.cdata)
+            self.T=float(baroreflex.simulation.basal_heart_period.cdata)
+            self.T_systole=float(baroreflex.simulation.duration_of_systole.cdata)
+            self.T_diastole = self.T-self.T_systole
+            self.counter_diastole = int(self.T_diastole/self.dt)
+            self.counter_systole = int(self.T_systole/self.dt)
+            self.baroreceptor_counter = self.counter_diastole
+            self.activation_level=0.0
+
+            # afferent pathway (baroreceptor control)
+            self.bc_max = float(baroreflex.afferent.y_max.cdata)
+            self.bc_min =  float(baroreflex.afferent.y_min.cdata)
+            self.bc_mid = float((self.bc_max+self.bc_min)/2)
+            self.slope = float(baroreflex.slope.cdata)
+            self.P_n = float(baroreflex.P_n.cdata)
+
+            #efferent pathway
+            self.T_prime=self.T
+            self.delta_T_prime=0.0
+            self.delta_c_prime=0.0
+            self.G_T = float(baroreflex.efferent.G_T.cdata)
+            self.G_k1 = float(baroreflex.efferent.G_k1.cdata)
+
+        if (self.baro_scheme=="Ursino_1998"):
+
+            #Activation function
+            self.dt=float(baroreflex.simulation.time_step.cdata)
+            self.T=float(baroreflex.simulation.basal_heart_period.cdata)
+            self.T_systole=float(baroreflex.simulation.duration_of_systole.cdata)
+            self.T_diastole = self.T-self.T_systole
+            self.counter_diastole = int(self.T_diastole/self.dt)
+            self.counter_systole = int(self.T_systole/self.dt)
+            self.baroreceptor_counter = self.counter_diastole
+            self.activation_level=0.0
+
+            #Afferent pathway
+            self.tau_p=float(baroreflex.afferent.tau_p.cdata)
+            self.tau_z=float(baroreflex.afferent.tau_z.cdata)
+            self.f_min=float(baroreflex.afferent.f_min.cdata)
+            self.f_max=float(baroreflex.afferent.f_max.cdata)
+            self.P_n=float(baroreflex.afferent.P_n.cdata)
+            self.k_a=float(baroreflex.afferent.k_a.cdata)
+            self.P_tilda=100.0
+
+            #Effernet sympathetic pathway
+            self.f_es_inf=float(baroreflex.efferent_sym.f_es_inf.cdata)
+            self.f_es_0=float(baroreflex.efferent_sym.f_es_0.cdata)
+            self.k_es=float(baroreflex.efferent_sym.k_es.cdata)
+            self.f_es=np.array([])
+
+            #Efferent parasympathetic (vagal) pathway
+            self.f_ev_inf=float(baroreflex.efferent_vagal.f_ev_inf.cdata)
+            self.f_ev_0=float(baroreflex.efferent_vagal.f_ev_0.cdata)
+            self.k_ev=float(baroreflex.efferent_vagal.k_ev.cdata)
+            self.f_cs_0=float(baroreflex.efferent_vagal.f_cs_0.cdata)
+            self.f_ev=np.array([])
+
+            #Regulation effector for heart period and contractility
+            #Sympathetic activity
+            self.G_Ts=float(baroreflex.regulation_sym.G_Ts.cdata)
+            self.D_Ts_in_second=float(baroreflex.regulation_sym.D_Ts.cdata)
+            self.D_Ts = int(self.D_Ts_in_second / self.dt)
+            self.tau_Ts=float(baroreflex.regulation_sym.tau_Ts.cdata)
+            self.delta_Ts=0.0
+            #contractility
+            self.G_E = float(baroreflex.regulation_sym.G_elastance.cdata)
+            self.G_L = float(baroreflex.regulation_sym.G_L.cdata)
+            self.G_k3 = float(baroreflex.regulation_sym.G_k3.cdata)
+            self.G_kcb = float(baroreflex.regulation_sym.G_kcb.cdata)
+            self.G_k_force = float(baroreflex.regulation_sym.G_k_force.cdata)
+
+            self.D_E_in_second=float(baroreflex.regulation_sym.D_elastance.cdata)
+            self.D_E=int(self.D_E_in_second/self.dt)
+
+            self.tau_E=float(baroreflex.regulation_sym.tau_elastance.cdata)
+            self.tau_L = float(baroreflex.regulation_sym.tau_L.cdata)
+            self.tau_k3 = float(baroreflex.regulation_sym.tau_k3.cdata)
+            self.tau_kcb = float(baroreflex.regulation_sym.tau_kcb.cdata)
+            self.tau_k_force = float(baroreflex.regulation_sym.tau_k_force.cdata)
+            self.E_0=float(baroreflex.regulation_sym.initial_elastance.cdata)
+            self.delta_E0=0.0
+            #Parasympathetic (vagal) activity
+            self.G_Tv=float(baroreflex.regulation_vagal.G_Tv.cdata)
+            self.D_Tv_in_second=float(baroreflex.regulation_vagal.D_Tv.cdata)
+            self.D_Tv = int(self.D_Tv_in_second / self.dt)
+            self.tau_Tv=float(baroreflex.regulation_vagal.tau_Tv.cdata)
+            self.delta_Tv=0.0
+
+
+            #Initial condition for heart period change
+            self.delta_Ti=[self.delta_Ts,self.delta_Tv]
+            self.T0=0.833
+            self.T_prime=0.833
+            self.data_buffer_size = data_buffer_size
+            self.sys_time=0.0
+            self.data_buffer_index = 0
+            self.sys_data = pd.DataFrame({'heart_period':
+                                              np.zeros(self.data_buffer_size),
+                                          'T_prime':
+                                              np.zeros(self.data_buffer_size),
+                                          'P_tilda':
+                                              np.zeros(self.data_buffer_size),
+                                          'f_cs':
+                                              np.zeros(self.data_buffer_size),
+    #                                      'f_es':
+    #                                          np.zeros(self.data_buffer_size),
+    #                                      'f_es_min':
+    #                                          np.zeros(self.data_buffer_size),
+    #                                      'f_ev':
+    #                                          np.zeros(self.data_buffer_size),
+    #                                      'delta_Ts':
+    #                                          np.zeros(self.data_buffer_size),
+    #                                      'delta_Tv':
+    #                                          np.zeros(self.data_buffer_size),
+    #                                      'LV_elastance':
+    #                                          np.zeros(self.data_buffer_size)
+                                            'L_scale_factor':
+                                                np.zeros(self.data_buffer_size),
+                                            'k_3_scale_factor':
+                                                np.zeros(self.data_buffer_size),
+                                            'k_cb_scale_factor':
+                                                np.zeros(self.data_buffer_size),
+                                            'k_force_scale_factor':
+                                                np.zeros(self.data_buffer_size)})
+            self.sys_data.at[0, 'heart_period'] = self.T
+            self.sys_data.at[0, 'T_prime'] = self.T
+            self.sys_data.at[0, 'P_tilda'] = 0
+            self.sys_data.at[0, 'f_cs'] = 0
+    #        self.sys_data.at[0, 'f_es'] = 0
+    #        self.sys_data.at[0, 'f_es_min'] = 0
+    #        self.sys_data.at[0, 'f_ev'] = 0
+    #        self.sys_data.at[0, 'delta_Ts'] = 0
+    #        self.sys_data.at[0, 'delta_Tv'] = 0
+    #        self.sys_data.at[0, 'LV_elastance'] = 2.392
+            self.sys_data.at[0, 'L_scale_factor'] = 1
+            self.sys_data.at[0, 'k_3_scale_factor'] = 1
+            self.sys_data.at[0, 'k_cb_scale_factor'] = 1
+            self.sys_data.at[0, 'k_force_scale_factor'] = 1
