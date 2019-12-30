04e60d615a20fbf3f084240c54db99c8cbc026cd
Hossein <42911295+HosseinGit@users.noreply.github.com>
2019-12-29 22:46:37 -0500

    Ventricle mass and slack volume can grow now
    Ventricle mass and slack volume can grow now. The rate of change of ventricle mass and slack volume can be discussed with Dr. Campbell.

--- Python_code/modules/Growth/growth.py
+++ Python_code/modules/Growth/growth.py
@@ -4,29 +4,72 @@ import pandas as pd
 class growth():
     """"Class for growth"""
     from .implement import return_force_array_per_cycle,steady_state_identifier
-    from .implement import update_data_holder
+    from .implement import return_ventricle_slack_volume,return_ventricle_mass,update_data_holder
     from .display import display_growth
-    def __init__(self, initial_T, time_step, growth_params,data_buffer_size):
+    def __init__(self,growth_params,data_buffer_size):
 
         growth = growth_params
 
-        self.memory = int(growth.memory.cdata)
-        self.force_counter = int(initial_T / time_step)
-        self.total_force_array = np.zeros(self.force_counter+1)
+
+        self.memory = int(growth.stress_signal.memory.cdata)
+        self.f_cirt = float(growth.stress_signal.critical_stress.cdata)
+        #lv_volume = float(growth.stress_signal.ventricle_slack_volume.cdata)
+        #internal_r = np.power((3.0 * 0.001 * lv_volume)/
+        #                        (2.0 * np.pi), (1.0 / 3.0))
+        #internal_area = 2.0 * np.pi * np.power(internal_r, 2.0)
+
+#        self.tw_0 = initial_wall_thickness#0.001 * lv_volume / internal_area
+#        self.tw= self.tw_0
+
+#        self.G_tw = float(growth.stress_signal.G_wall_thickness.cdata)
+#        self.tau_tw = float(growth.stress_signal.tau_wall_thickness.cdata)
+#        self.delta_tw = [0.0]
+
+        #ventricle_mass
+        self.ventricle_wall_volume = \
+        float(growth.stress_signal.ventricle_mass.ventricle_wall_volume.cdata)
+        self.w_vol_0 = self.ventricle_wall_volume#float(growth.stress_signal.ventricle_wall_volume.cdata)
+        #self.ventricle_wall_volume = self.w_vol_0
+        self.G_w_vol = \
+        float(growth.stress_signal.ventricle_mass.G_wall_volume.cdata)
+        self.tau_w_vol = \
+        float(growth.stress_signal.ventricle_mass.tau_wall_volume.cdata)
+        self.delta_w_vol = [0.0]
+        #ventricle_slack_volume
+        self.ventricle_slack_volume = \
+        float(growth.stress_signal.ventricle_volume.ventricle_slack_volume.cdata)
+        self.sl_vol_0 = self.ventricle_slack_volume
+        self.G_sl_vol = \
+        float(growth.stress_signal.ventricle_volume.G_slack_volume.cdata)
+        self.tau_sl_vol = \
+        float(growth.stress_signal.ventricle_volume.tau_slack_volume.cdata)
+        self.delta_sl_vol = [0.0]
+        #self.force_counter = int(initial_T / time_step)
+        #self.total_force_array = np.zeros(self.force_counter+1)
         #self.avg_total_force_array = np.zeros(self.memory+1)
-        self.avg_total_force_array = np.array([0])
+        #self.avg_total_force_array = np.array([0])
 
 
-        self.counter = self.force_counter
-        self.avg_counter = 0
-
+        #self.counter = self.force_counter
+        #self.avg_counter = 0
+        #data
         self.data_buffer_size = data_buffer_size
-        self.sys_time = 0.0
+        self.gr_time = 0.0
         self.data_buffer_index = 0
         self.gr_data = pd.DataFrame({'average_force':
-                                    np.zeros(self.data_buffer_size),
+                                            np.zeros(self.data_buffer_size),
                                     'cycle_counter':
-                                    np.zeros(self.data_buffer_size)})
+                                            np.zeros(self.data_buffer_size),
+                                    'lv_wall_thickness':
+                                            np.zeros(self.data_buffer_size),
+                                    'ventricle_wall_volume':
+                                            np.zeros(self.data_buffer_size),
+                                    'ventricle_slack_volume':
+                                            np.zeros(self.data_buffer_size)})
+
 
         self.gr_data.at[0, 'average_force'] = 0
         self.gr_data.at[0, 'cycle_counter'] = 0
+        self.gr_data.at[0, 'wall_thickness'] = 0#self.tw_0
+        self.gr_data.at[0, 'ventricle_wall_volume'] = self.ventricle_wall_volume
+        self.gr_data.at[0, 'ventricle_slack_volume'] = self.ventricle_slack_volume
--- Python_code/modules/Growth/implement.py
+++ Python_code/modules/Growth/implement.py
@@ -1,5 +1,58 @@
 import numpy as np
+from scipy.integrate import solve_ivp
 import pandas as pd
+
+
+
+def return_lv_wall_thickness(self,time_step,cell_stress):
+
+    f=cell_stress
+    if f > self.f_cirt:
+
+        def derivs(y,t):
+            delta_tw = np.zeros(1)
+            delta_tw[0] = 1/self.tau_tw*self.G_tw*(f-self.f_cirt)*self.tw_0
+            return delta_tw
+        initial_conditions = self.delta_tw
+        sol = solve_ivp(derivs,[0,time_step],initial_conditions)
+        self.delta_tw=sol.y[:,-1]
+        self.tw = self.tw_0 + self.delta_tw
+    else:
+        self.tw=self.tw
+    return self.tw
+def return_ventricle_mass(self,time_step,cell_stress):
+
+    f=cell_stress
+    if f > self.f_cirt:
+
+        def derivs(y,t):
+            delta_wall_vol = np.zeros(1)
+            delta_wall_vol[0] = 1/self.tau_w_vol*self.G_w_vol*(f-self.f_cirt)*self.w_vol_0
+            return delta_wall_vol
+        initial_conditions = self.delta_w_vol
+        sol = solve_ivp(derivs,[0,time_step],initial_conditions)
+        self.delta_w_vol=sol.y[:,-1]
+        self.ventricle_wall_volume = self.w_vol_0 + self.delta_w_vol
+    else:
+        self.ventricle_wall_volume=self.ventricle_wall_volume
+    return self.ventricle_wall_volume
+
+def return_ventricle_slack_volume(self,time_step,cell_stress):
+
+    f=cell_stress
+    if f > self.f_cirt:
+        def derivs(y,t):
+            delta_slack_vol = np.zeros(1)
+            delta_slack_vol[0] = 1/self.tau_sl_vol*self.G_sl_vol*(f-self.f_cirt)*self.sl_vol_0
+            return delta_slack_vol
+        initial_conditions = self.delta_sl_vol
+        sol = solve_ivp(derivs,[0,time_step],initial_conditions)
+        self.delta_sl_vol=sol.y[:,-1]
+        self.ventricle_slack_volume = self.sl_vol_0 + self.delta_sl_vol
+    else:
+        self.ventricle_wall_volume=self.ventricle_wall_volume
+    return self.ventricle_wall_volume
+
 
 def steady_state_identifier(self):
 
@@ -10,29 +63,16 @@ def steady_state_identifier(self):
         self.steady_state = True
     else:
         self.steady_state = False
-    #print(self.steady_state)
+
 def avg_total_force (self):
     print('self.avg_counter',self.avg_counter)
 
     self.avg_total_force_array=np.append(self.avg_total_force_array,
                                 np.mean(self.total_force_array))
     self.avg_counter +=1
-    """if self.avg_counter <= self.memory:
 
-        self.avg_total_force_array[self.avg_counter] =\
-         np.mean(self.total_force_array)
-        self.avg_counter += 1
-
-    else:
-
-        self.avg_total_force_array = \
-        np.roll(self.avg_total_force_array,-1)
-
-        self.avg_total_force_array[self.memory] =\
-         np.mean(self.total_force_array)
-
-        self.avg_counter +=1"""
     #print(self.avg_total_force_array)
+
 def return_force_array_per_cycle(self,time_step,heart_period,total_force):
 
 
@@ -44,15 +84,18 @@ def return_force_array_per_cycle(self,time_step,heart_period,total_force):
         self.total_force_array=np.zeros(self.force_counter+1)
         return
     i=self.force_counter - self.counter
-    #print(i)
+
     self.total_force_array[i] = total_force
 
     self.counter -= 1
 
 def update_data_holder(self,time_step):
 
-    self.sys_time = self.sys_time + time_step
+    self.gr_time = self.gr_time + time_step
     self.data_buffer_index += 1
-    self.gr_data.at[self.data_buffer_index, 'average_force']=\
-                                    self.avg_total_force_array[-1]
-    self.gr_data.at[self.data_buffer_index, 'cycle_counter']= self.avg_counter
+    #self.gr_data.at[self.data_buffer_index, 'average_force']=\
+    #                                self.avg_total_force_array[-1]
+    #self.gr_data.at[self.data_buffer_index, 'cycle_counter']= self.avg_counter
+    #self.gr_data.at[self.data_buffer_index, 'wall_thickness'] = self.tw
+    self.gr_data.at[self.data_buffer_index, 'ventricle_wall_volume'] = self.ventricle_wall_volume
+    self.gr_data.at[self.data_buffer_index, 'ventricle_slack_volume'] = self.ventricle_slack_volume
