f499ae39bc79b644e8f724ce122e3c12b5d6a38f
Hossein <42911295+HosseinGit@users.noreply.github.com>
2020-01-13 20:04:16 -0500

    Eccentric and concentric growth works now
    Both types of stress driven ventricle growth are implemented and working properly.

--- Python_code/modules/Growth/growth.py
+++ Python_code/modules/Growth/growth.py
@@ -4,54 +4,33 @@ import pandas as pd
 class growth():
     """"Class for growth"""
     from .implement import return_force_array_per_cycle,steady_state_identifier
-    from .implement import return_ventricle_slack_volume,return_ventricle_mass,update_data_holder
+    from .implement import return_number_of_hs,return_lv_mass,update_data_holder
     from .display import display_growth
-    def __init__(self,growth_params,data_buffer_size):
+    def __init__(self,growth_params,initial_numbers_of_hs,data_buffer_size):
 
         growth = growth_params
 
 
         self.memory = int(growth.stress_signal.memory.cdata)
-        self.f_cirt = float(growth.stress_signal.critical_stress.cdata)
-        #lv_volume = float(growth.stress_signal.ventricle_slack_volume.cdata)
-        #internal_r = np.power((3.0 * 0.001 * lv_volume)/
-        #                        (2.0 * np.pi), (1.0 / 3.0))
-        #internal_area = 2.0 * np.pi * np.power(internal_r, 2.0)
+        self.f_cirt = float(growth.stress_signal.critical_total_stress.cdata)
+        self.pas_cirt = float(growth.stress_signal.critical_passive_stress.cdata)
 
-#        self.tw_0 = initial_wall_thickness#0.001 * lv_volume / internal_area
-#        self.tw= self.tw_0
-
-#        self.G_tw = float(growth.stress_signal.G_wall_thickness.cdata)
-#        self.tau_tw = float(growth.stress_signal.tau_wall_thickness.cdata)
-#        self.delta_tw = [0.0]
-
-        #ventricle_mass
+        #Concentric growth (ventricle_mass)
         self.ventricle_wall_volume = \
-        float(growth.stress_signal.ventricle_mass.ventricle_wall_volume.cdata)
+        float(growth.stress_signal.concenrtric_growth.ventricle_wall_volume.cdata)
         self.w_vol_0 = self.ventricle_wall_volume#float(growth.stress_signal.ventricle_wall_volume.cdata)
         #self.ventricle_wall_volume = self.w_vol_0
         self.G_w_vol = \
-        float(growth.stress_signal.ventricle_mass.G_wall_volume.cdata)
+        float(growth.stress_signal.concenrtric_growth.G_wall_volume.cdata)
         self.tau_w_vol = \
-        float(growth.stress_signal.ventricle_mass.tau_wall_volume.cdata)
+        float(growth.stress_signal.concenrtric_growth.tau_wall_volume.cdata)
         self.delta_w_vol = [0.0]
-        #ventricle_slack_volume
-        self.ventricle_slack_volume = \
-        float(growth.stress_signal.ventricle_volume.ventricle_slack_volume.cdata)
-        self.sl_vol_0 = self.ventricle_slack_volume
-        self.G_sl_vol = \
-        float(growth.stress_signal.ventricle_volume.G_slack_volume.cdata)
-        self.tau_sl_vol = \
-        float(growth.stress_signal.ventricle_volume.tau_slack_volume.cdata)
-        self.delta_sl_vol = [0.0]
-        #self.force_counter = int(initial_T / time_step)
-        #self.total_force_array = np.zeros(self.force_counter+1)
-        #self.avg_total_force_array = np.zeros(self.memory+1)
-        #self.avg_total_force_array = np.array([0])
 
+        #Eccentric growth (number of hs in sereis)
+        self.n_of_hs_0 = initial_numbers_of_hs
+        self.G_n_hs = float(growth.stress_signal.eccenrtric_growth.G_number_of_hs.cdata)
+        self.delta_n_hs = [0.0]
 
-        #self.counter = self.force_counter
-        #self.avg_counter = 0
         #data
         self.data_buffer_size = data_buffer_size
         self.gr_time = 0.0
@@ -60,16 +39,12 @@ class growth():
                                             np.zeros(self.data_buffer_size),
                                     'cycle_counter':
                                             np.zeros(self.data_buffer_size),
-                                    'lv_wall_thickness':
-                                            np.zeros(self.data_buffer_size),
                                     'ventricle_wall_volume':
                                             np.zeros(self.data_buffer_size),
-                                    'ventricle_slack_volume':
+                                    'lv_wall_thickness':
+                                            np.zeros(self.data_buffer_size),
+                                    'number_of_hs':
                                             np.zeros(self.data_buffer_size)})
 
-
-        self.gr_data.at[0, 'average_force'] = 0
-        self.gr_data.at[0, 'cycle_counter'] = 0
-        self.gr_data.at[0, 'wall_thickness'] = 0#self.tw_0
         self.gr_data.at[0, 'ventricle_wall_volume'] = self.ventricle_wall_volume
-        self.gr_data.at[0, 'ventricle_slack_volume'] = self.ventricle_slack_volume
+        self.gr_data.at[0, 'number_of_hs'] = self.n_of_hs_0
--- Python_code/modules/Growth/implement.py
+++ Python_code/modules/Growth/implement.py
@@ -20,7 +20,7 @@ def return_lv_wall_thickness(self,time_step,cell_stress):
     else:
         self.tw=self.tw
     return self.tw
-def return_ventricle_mass(self,time_step,cell_stress):
+def return_lv_mass(self,time_step,cell_stress):
 
     f=cell_stress
 #    if f > self.f_cirt:
@@ -37,21 +37,34 @@ def return_ventricle_mass(self,time_step,cell_stress):
 #        self.ventricle_wall_volume=self.ventricle_wall_volume
     return self.ventricle_wall_volume
 
-def return_ventricle_slack_volume(self,time_step,cell_stress):
+def return_number_of_hs(self,time_step,passive_stress):
+    f= passive_stress
 
-    f=cell_stress
+    def derivs(y,t):
+        delta_number_of_hs = np.zeros(1)
+        delta_number_of_hs[0] = self.G_n_hs * self.n_of_hs_0 * (f - self.pas_cirt)
+        return delta_number_of_hs
+    initial_conditions = self.delta_n_hs
+    sol = solve_ivp(derivs,[0,time_step],initial_conditions)
+    self.delta_n_hs = sol.y[:,-1]
+    self.n_of_hs = self.n_of_hs_0 + self.delta_n_hs
+
+    return self.n_of_hs
+def return_lv_slack_volume_change(self,time_step,cell_stretch):
+
+    s=cell_stretch
 #    if f > self.f_cirt:
     def derivs(y,t):
         delta_slack_vol = np.zeros(1)
-        delta_slack_vol[0] = 1/self.tau_sl_vol*self.G_sl_vol*(f-self.f_cirt)*self.sl_vol_0
+        delta_slack_vol[0] = 1/self.tau_sl_vol*self.G_sl_vol*(s-self.s_cirt)*self.sl_vol_0
         return delta_slack_vol
     initial_conditions = self.delta_sl_vol
     sol = solve_ivp(derivs,[0,time_step],initial_conditions)
     self.delta_sl_vol=sol.y[:,-1]
-    self.ventricle_slack_volume = self.sl_vol_0 + self.delta_sl_vol
+    #self.ventricle_slack_volume = self.sl_vol_0 + self.delta_sl_vol
 #    else:
 #        self.ventricle_slack_volume=self.ventricle_slack_volume
-    return self.ventricle_slack_volume
+    return self.delta_sl_vol
 
 
 def steady_state_identifier(self):
@@ -93,9 +106,6 @@ def update_data_holder(self,time_step):
 
     self.gr_time = self.gr_time + time_step
     self.data_buffer_index += 1
-    #self.gr_data.at[self.data_buffer_index, 'average_force']=\
-    #                                self.avg_total_force_array[-1]
-    #self.gr_data.at[self.data_buffer_index, 'cycle_counter']= self.avg_counter
-    #self.gr_data.at[self.data_buffer_index, 'wall_thickness'] = self.tw
+
     self.gr_data.at[self.data_buffer_index, 'ventricle_wall_volume'] = self.ventricle_wall_volume
-    self.gr_data.at[self.data_buffer_index, 'ventricle_slack_volume'] = self.ventricle_slack_volume
+    self.gr_data.at[self.data_buffer_index, 'number_of_hs'] = self.n_of_hs
