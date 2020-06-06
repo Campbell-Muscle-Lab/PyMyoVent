80ff81fc04a0e1ab63a6d0d52740646c8ef6bbad
Hossein <42911295+HosseinGit@users.noreply.github.com>
2020-06-05 23:58:48 -0400

    Most updated

--- Python_code/modules/MyoSim/half_sarcomere/membranes/membranes.py
+++ Python_code/modules/MyoSim/half_sarcomere/membranes/membranes.py
@@ -7,11 +7,9 @@ from .Ten_Tusscher_2004 import computeRates_with_activation as \
 tt_computeRates_with_activation
 from .Ten_Tusscher_2004 import initConsts_with_adjustments as \
 tt_initConsts_with_adjustments
-from .Ten_Tusscher_2004 import computeRates as tt_computeRates
-from .Ten_Tusscher_2004 import initConsts as tt_initConsts
+from .Ten_Tusscher_2004 import compureRatesonly as tt_computeRatesonly
 from .Shannon_Bers_2004 import initConsts as sb_initConsts
 from .Shannon_Bers_2004 import computeRates as sb_computeRates
-from .Shannon_Bers_2004 import solve_system as sb_solve_system
 from .Grandi_2009 import initConsts as g_initConsts
 from .Grandi_2009 import computeRates as g_coputeRates
 
@@ -25,10 +23,11 @@ class membranes():
 
         # Set up the rates and the y vector which are kinetics specific
         if (self.kinetic_scheme == "simple_2_compartment"):
-            self.Ca_content = float(membrane_params["Ca_content"][0])
-            self.k_leak = float(membrane_params["k_leak"][0])
-            self.k_act = float(membrane_params["k_act"][0])
-            self.k_serca = float(membrane_params["k_serca"][0])
+            temp = membrane_params["simple_2_compartment"]
+            self.Ca_content = float(temp["Ca_content"][0])
+            self.k_leak = float(temp["k_leak"][0])
+            self.k_act = float(temp["k_act"][0])
+            self.k_serca = float(temp["k_serca"][0])
 
             self.y = np.zeros(2)
             self.y[1] = self.Ca_content
@@ -37,24 +36,24 @@ class membranes():
 
         if (self.kinetic_scheme == "Ten_Tusscher_2004"):
             # Adjust membrane factors
+            temp = membrane_params["Ten_Tusscher_2004"]
             membrane_factors = dict();
             membrane_factors['g_to'] = \
-                float(membrane_params["g_to_factor"][0]) #const 20
+                float(temp["g_to_factor"][0]) #const 20
             membrane_factors['g_Kr'] = \
-                float(membrane_params["g_Kr_factor"][0]) #const 14
+                float(temp["g_Kr_factor"][0]) #const 14
             membrane_factors['g_Ks'] = \
-                float(membrane_params["g_Ks_factor"][0]) #const 15
+                float(temp["g_Ks_factor"][0]) #const 15
             membrane_factors['Ca_a_rel'] = \
-                float(membrane_params["Ca_a_rel_factor"][0]) #const 34
+                float(temp["Ca_a_rel_factor"][0]) #const 34
             membrane_factors['Ca_V_leak'] = \
-                float(membrane_params["Ca_V_leak_factor"][0]) #const 38
+                float(temp["Ca_V_leak_factor"][0]) #const 38
             membrane_factors['Ca_Vmax_up'] = \
-                float(membrane_params["Ca_Vmax_up_factor"][0]) #const 39
+                float(temp["Ca_Vmax_up_factor"][0]) #const 39
             membrane_factors['g_CaL'] = \
-                float(membrane_params["g_CaL_factor"][0]) #const 18
+                float(temp["g_CaL_factor"][0]) #const 18
             (self.y, self.constants) = \
-                tt_initConsts()
-                #tt_initConsts_with_adjustments(membrane_factors)
+                tt_initConsts_with_adjustments(membrane_factors)
 
             # Ten_Tusscher model assumese Ca_conc is in mM
             self.myofilament_Ca_conc = 0.001*self.y[3]
@@ -66,10 +65,6 @@ class membranes():
 
         if (self.kinetic_scheme=="Grandi_2009"):
             (self.y, self.constants)=g_initConsts()
-
-
-
-
 
     def evolve_kinetics(self, time_step, activation):
         """ evolves kinetics """
@@ -98,16 +93,13 @@ class membranes():
                                     activation=activation),
                             [0, 1000*time_step], self.y,
                             method='BDF')
-            #sol = solve_ivp(partial(tt_computeRates,
-            #                        constants=self.constants,activation=activation),
-            #                [0, 1000*time_step], self.y,
-            #                method='BDF')
-            #sol = solve_ivp(partial(tt_computeRates,
-            #                        constants=self.constants),[0, 1000*time_step], self.y,method='BDF')
+#            sol = solve_ivp(partial(tt_computeRatesonly,constants=self.constants,activation=activation),
+#                            [0, 1000*time_step], self.y,
+#                            method='BDF')
             self.y = sol.y[:, -1]
             # Ten_Tusscher model assumese Ca_conc is in mM
+#            self.myofilament_Ca_conc = 0.001*self.y[3]
             self.myofilament_Ca_conc = 0.001*self.y[3]
-
         if (self.kinetic_scheme=="Shannon_Bers_2004"):
 
             sol=solve_ivp(partial(sb_computeRates,
